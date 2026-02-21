62e951e5a6e88955aad97a269b354f5c5db4906f
harlan <harlan.victor@liven.tech>
2026-02-21 12:59:05 -0300

    fix: teste de empresa

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from silver.mapa_controle_operacional import run as silver_mapa_controle_operaci
 from silver.tipo_dia import run as silver_tipo_dia
 
 from gold.dim_data import run as dim_data
+from gold.dim_empresa import run as dim_empresa
 # from gold.dim_empresa import run as dim_empresa
 # from gold.dim_tempo import run as dim_tempo
 # from gold.dim_justificativa import run as dim_justificativa
@@ -23,20 +24,20 @@ def main():
     spark = get_spark()
     # spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
     # Bronze [check]
-    bronze_interrupcao_viagem(spark, RAW_PATH, BRONZE_PATH)
+    # bronze_interrupcao_viagem(spark, RAW_PATH, BRONZE_PATH)
     # bronze_operadora(spark, RAW_PATH, BRONZE_PATH)
-    bronze_mco(spark, RAW_PATH, BRONZE_PATH)
+    # bronze_mco(spark, RAW_PATH, BRONZE_PATH)
     # bronze_tipo_dia(spark, RAW_PATH, BRONZE_PATH)
     
     # Silver [doing]
-    silver_empresa_operadora(spark, SILVER_PATH)
-    silver_interrupcao_viagem(spark, SILVER_PATH)
-    silver_mapa_controle_operacional(spark, SILVER_PATH)
-    silver_tipo_dia(spark, SILVER_PATH)
+    # silver_empresa_operadora(spark, SILVER_PATH)
+    # silver_interrupcao_viagem(spark, SILVER_PATH)
+    # silver_mapa_controle_operacional(spark, SILVER_PATH)
+    # silver_tipo_dia(spark, SILVER_PATH)
     
     # Gold[pendente]
     dim_data(spark, GOLD_PATH)
-    # dim_empresa(spark, SILVER_PATH, GOLD_PATH)
+    dim_empresa(spark, SILVER_PATH, GOLD_PATH)
     # dim_tempo(spark, SILVER_PATH, GOLD_PATH)
     # dim_justificativa(spark, SILVER_PATH, GOLD_PATH)
 
